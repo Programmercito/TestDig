01f6ab9404c629b24935a3a73618599ad365c34f
Programmercito <osboceo@live.com>
2021-03-14 16:09:04 -0400

    eliminando cosas innecesarias

--- TestDigitalWeb/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/TestDigitalWeb .
-call docker rm -f TestDigitalWeb
-call docker run -d -p 9080:9080 -p 9443:9443 --name TestDigitalWeb com.mycompany/TestDigitalWeb
--- TestDigitalWeb/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.mycompany/TestDigitalWeb .
-docker rm -f TestDigitalWeb || true && docker run -d -p 9080:9080 -p 9443:9443 --name TestDigitalWeb com.mycompany/TestDigitalWeb
